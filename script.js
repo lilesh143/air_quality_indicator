5856415ef5de8fc10679aaa45273670a3f0fd973
Lilesh Bawane <lileshrocking@gmail.com>
2024-02-22 15:28:33 +0530

    initial commit

--- script.js
+++ script.js
@@ -6,92 +6,97 @@ const airQualityStat = document.querySelector(".air-quality-status")
 const srchBtn = document.querySelector(".search-btn")
 const componentsEle = document.querySelectorAll(".component-val")
 
-const appId = "" // Get your own API Key from https://home.openweathermap.org/api_keys
-const link = "https://api.openweathermap.org/data/2.5/air_pollution"	// API end point
+const appId = "8774b26414ab041bce77e51bcf64f61f" // Get your own API Key from https://home.openweathermap.org/api_keys
+const link = "https://api.openweathermap.org/data/2.5/air_pollution" // API end point
+    // https://api.openweathermap.org/data/2.5/air_pollution
 
 const getUserLocation = () => {
-	// Get user Location
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(onPositionGathered, onPositionGatherError)
-	} else {
-		onPositionGatherError({ message: "Can't Access your location. Please enter your co-ordinates" })
-	}
+    // Get user Location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onPositionGathered, onPositionGatherError)
+    } else {
+        onPositionGatherError({ message: "Can't Access your location. Please enter your co-ordinates" })
+    }
 }
 
 const onPositionGathered = (pos) => {
-	let lat = pos.coords.latitude.toFixed(4), lon = pos.coords.longitude.toFixed(4)
+    let lat = pos.coords.latitude.toFixed(4),
+        lon = pos.coords.longitude.toFixed(4)
 
-	// Set values of Input for user to know
-	latInp.value = lat
-	lonInp.value = lon
+    // Set values of Input for user to know
+    latInp.value = lat
+    lonInp.value = lon
 
-	// Get Air data from weather API
-	getAirQuality(lat, lon)
+    // Get Air data from weather API
+    getAirQuality(lat, lon)
 }
 
-const getAirQuality = async (lat, lon) => {
-	// Get data from api
-	const rawData = await fetch(`${link}?lat=${lat}&lon=${lon}&appid=${appId}`).catch(err => {
-		onPositionGatherError({ message: "Something went wrong. Check your internet conection." })
-		console.log(err)
-	})
-	const airData = await rawData.json()
-	setValuesOfAir(airData)
-	setComponentsOfAir(airData)
+const getAirQuality = async(lat, lon) => {
+    // Get data from api
+    const rawData = await fetch(`${link}?lat=${lat}&lon=${lon}&appid=${appId}`).catch(err => {
+        onPositionGatherError({ message: "Something went wrong. Check your internet conection." })
+        console.log(err)
+    })
+    const airData = await rawData.json()
+    console.log(airData);
+
+    setValuesOfAir(airData)
+    setComponentsOfAir(airData)
 }
 
 const setValuesOfAir = airData => {
-	const aqi = airData.list[0].main.aqi
-	let airStat = "", color = ""
+    const aqi = airData.list[0].main.aqi
+    let airStat = "",
+        color = ""
 
-	// Set Air Quality Index
-	airQuality.innerText = aqi
+    // Set Air Quality Index
+    airQuality.innerText = aqi
 
-	// Set status of air quality
+    // Set status of air quality
 
-	switch (aqi) {
-		case 1:
-			airStat = "Good"
-			color = "rgb(19, 201, 28)"
-			break
-			case 2:
-				airStat = "Fair"
-				color = "rgb(15, 134, 25)"
-				break
-			case 3:
-				airStat = "Moderate"
-				color = "rgb(201, 204, 13)"
-				break
-			case 4:
-				airStat = "Poor"
-				color = "rgb(204, 83, 13)"
-				break
-		case 5:
-			airStat = "Very Poor"
-			color = "rgb(204, 13, 13)"
-			break
-		default:
-			airStat = "Unknown"
-	}
+    switch (aqi) {
+        case 1:
+            airStat = "Good"
+            color = "rgb(19, 201, 28)"
+            break
+        case 2:
+            airStat = "Fair"
+            color = "rgb(15, 134, 25)"
+            break
+        case 3:
+            airStat = "Moderate"
+            color = "rgb(201, 204, 13)"
+            break
+        case 4:
+            airStat = "Poor"
+            color = "rgb(204, 83, 13)"
+            break
+        case 5:
+            airStat = "Very Poor"
+            color = "rgb(204, 13, 13)"
+            break
+        default:
+            airStat = "Unknown"
+    }
 
-	airQualityStat.innerText = airStat
-	airQualityStat.style.color = color
+    airQualityStat.innerText = airStat
+    airQualityStat.style.color = color
 }
 
 const setComponentsOfAir = airData => {
-	let components = {...airData.list[0].components}
-	componentsEle.forEach(ele => {
-		const attr = ele.getAttribute('data-comp')
-		ele.innerText = components[attr] += " μg/m³"
-	})
+    let components = {...airData.list[0].components }
+    componentsEle.forEach(ele => {
+        const attr = ele.getAttribute('data-comp')
+        ele.innerText = components[attr] += " μg/m³"
+    })
 }
 
 const onPositionGatherError = e => {
-	errorLabel.innerText = e.message
+    errorLabel.innerText = e.message
 }
 
 srchBtn.addEventListener("click", () => {
-	getAirQuality(parseFloat(latInp.value).toFixed(4), parseFloat(lonInp.value).toFixed(4))
+    getAirQuality(parseFloat(latInp.value).toFixed(4), parseFloat(lonInp.value).toFixed(4))
 })
 
 getUserLocation()
